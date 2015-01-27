1c5bb540885ee673ab79e1ebd839144cc44a2c71
ivansabik <ivan.rodriguez.o@gmail.com>
2015-01-27 01:46:22 -0600

    jqmificado

--- js/home.js
+++ js/home.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    var URL_CONSULTA_PLACAS = '/app/placas.html#';
-    $('#consultar-placas').click(function(){
-        var placas = $('#placas').val().toUpperCase();
-        window.location.href = URL_CONSULTA_PLACAS + placas;
+$(document).on('pageshow',"#index",function(event){
+	$('#logo').click(function(){
+        $.mobile.changePage('index.html');
     });
     
+    $('#consultar-placas').click(function(){
+        var placas = $('#placas').val().toUpperCase();
+        if(typeof(Storage)!=='undefined') {
+			localStorage.placas=placas;            
+        }
+        $.mobile.changePage('placas.html');
+    });
 });

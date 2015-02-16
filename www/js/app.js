e1aaf8749efbfa130024ab062c9b6b782cf3ddc2
ivan sabik <ivan.rodriguez.o@gmail.com>
2015-02-15 18:17:08 -0600

    bowerizing, fixes html, funciona consulta placas

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,9 @@ NUM_MAX_RES = 5;
 PATH = window.location.href.replace('index.html', '');
 URL_GMAPS_MAPA = 'http://maps.google.com/maps?q=';
 URL_GMAPS_DIR = 'http://maps.google.com/maps?saddr=Current+Location&daddr=';
-URL_GMAPS_CORRALONES = 'https://www.google.com/maps/d/edit?mid=zQg_E1qdziCA.kLI2wjNXFpnc';
-URL_GMAPS_VERIFICENTROS = 'https://www.google.com/maps/d/edit?mid=zQg_E1qdziCA.kilFkwYKHw9s';
+URL_GMAPS_CORRALONES = 'http://goo.gl/K3YW7R';
+URL_GMAPS_VERIFICENTROS = 'http://goo.gl/4x9EtG';
+
 // Home
 $(document).on('pageshow', '#index', function () {
     var placas = simpleStorage.get('placas');
@@ -30,6 +31,7 @@ $(document).on('pageshow', '#index', function () {
         }
     });
 });
+
 // Consultar placas
 $(document).on('pageshow', '#consulta-placas', function () {
     $.mobile.loading('show');
@@ -78,6 +80,7 @@ $(document).on('pageshow', '#consulta-placas', function () {
         $.ajax(ajaxOptions);
     }
 });
+
 // Ver corralones cercanos
 $(document).on('pageshow', '#lista-corralones', function () {
     var corralones = JSON.parse(localStorage.corralones);
@@ -87,6 +90,7 @@ $(document).on('pageshow', '#lista-corralones', function () {
     }
     $.mobile.loading('hide');
 });
+
 $(document).on('pageshow', '#menu-corralones', function () {
     $('#ver-corralones-cercanos').click(function () {
         $.mobile.loading('show');
@@ -115,6 +119,7 @@ $(document).on('pageshow', '#menu-corralones', function () {
             }
         });
     });
+
     $('#ver-corralones-todos').click(function () {
         $.mobile.loading('show');
         $.getJSON(PATH + 'json/corralones.json', function (corralonesJson) {
@@ -125,42 +130,6 @@ $(document).on('pageshow', '#menu-corralones', function () {
             }
         });
     });
-    function errorCoordenadas() {
-        $.mobile.loading('hide');
-        alert('Err0r! Obtener coordenadas geolocalización');
-        return false;
-    }
-
-    function errorGeolocalizacion() {
-        $.mobile.loading('hide');
-        alert('Err0r! Error geolocalización!');
-        return false;
-    }
-
-    function obtenerDistancia(lat1, lon1, lat2, lon2) {
-        var R = 6371;
-        var dLat = convertirRadianes(lat2 - lat1);
-        var dLon = convertirRadianes(lon2 - lon1);
-        lat1 = convertirRadianes(lat1);
-        lat2 = convertirRadianes(lat2);
-        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c;
-        return d;
-    }
-
-    function convertirRadianes(numero) {
-        return numero * Math.PI / 180;
-    }
-
-    function compararDistancia(a, b) {
-        if (a.distancia < b.distancia)
-            return -1;
-        if (a.distancia > b.distancia)
-            return 1;
-        return 0;
-    }
 });
 // Ver todos los corralones
 
--- www/js/modelos.js
+++ www/js/modelos.js
@@ -32,103 +32,164 @@ function Vehiculo(json) {
         adeudos.push(adeudo);
     }
     this.adeudosTenencia = adeudos;
+
+    this.toHtml = function () {
+        var html = '<div class="cuadro-info">';
+        if (this.placas) {
+            html += '<span class="titulo">Placas: </span><span>' + this.placas + '</span></br>';
+        }
+        if (this.modelo) {
+            html += '<span class = "titulo" >Modelo: </span><span>' + this.modelo + '</span></br>';
+        }
+        if (this.procedencia) {
+            html += '<span class = "titulo" >Procedencia: </span><span>' + this.procedencia + '</span></br>';
+        }
+        if (this.valorFactura) {
+            html += '<span class = "titulo" >Valor factura: </span><span class="cacao">' + this.valorFactura + '</span></br>';
+        }
+        if (this.claveVehicular) {
+            html += '<span class = "titulo" >Clave vehicular: </span><span>' + this.claveVehicular + '</span></br>';
+        }
+        if (this.fechaFactura) {
+            html += '<span class = "titulo" >Fecha factura: </span><span>' + this.fechaFactura + '</span></br>';
+        }
+        if (this.rfc) {
+            html += '<span class = "titulo" >RFC: </span><span>' + this.rfc + '</span></br>';
+        }
+        if (this.depreciacion) {
+            html += '<span class = "titulo" >Monto depreciación: </span><span class="cacao">' + this.depreciacion + '</span></br>';
+        }
+        if (this.placas) {
+            html += '<span class = "titulo" >Total adeudos: </span><span class="cacao">' + this.totalAdeudos + '</span>';
+        }
+        html += '</div></br>';
+        return html;
+    };
 }
 
-
-Vehiculo.prototype.toHtml = function () {
-    var html = '<div class="cuadro-info">';
-    if (this.placas) {
-        html += '<span class="titulo">Placas: </span><span>' + this.placas + '</span></br>';
-    }
-    if (this.modelo) {
-        html += '<span class = "titulo" >Modelo: </span><span>' + this.modelo + '</span></br>';
-    }
-    if (this.procedencia) {
-        html += '<span class = "titulo" >Procedencia: </span><span>' + this.procedencia + '</span></br>';
-    }
-    if (this.valorFactura) {
-        html += '<span class = "titulo" >Valor factura: </span><span class="cacao">' + this.valorFactura + '</span></br>';
-    }
-    if (this.claveVehicular) {
-        html += '<span class = "titulo" >Clave vehicular: </span><span>' + this.claveVehicular + '</span></br>';
-    }
-    if (this.fechaFactura) {
-        html += '<span class = "titulo" >Fecha factura: </span><span>' + this.fechaFactura + '</span></br>';
-    }
-    if (this.rfc) {
-        html += '<span class = "titulo" >RFC: </span><span>' + this.rfc + '</span></br>';
-    }
-    if (this.depreciacion) {
-        html += '<span class = "titulo" >Monto depreciación: </span><span class="cacao">' + this.depreciacion + '</span></br>';
-    }
-    if (this.placas) {
-        html += '<span class = "titulo" >Total adeudos: </span><span class="cacao">' + this.totalAdeudos + '</span>';
-    }
-    html += '</div></br>';
-    return html;
-};
+// TODO: Regex para placas
 Vehiculo.validaPlacas = function (placas) {
     if (placas.length !== 6) {
         return false;
     }
     return true;
 };
-Vehiculo.prototype.guardaLocal = function () {
+
+Vehiculo.guardaLocal = function () {
 }; // asigna ttl y guarda
 
 Vehiculo.existeLocal = function (placas) {
 }; // busca si está guardado
 
 function Corralon() {
-}
-
-Corralon.prototype.toHtml = function () {
-    var html = '<div class="cuadro-info"> \
+    this.toHtml = function () {
+        var html = '<div class="cuadro-info"> \
                 <h4>' + this.nombre + '</h4> \
                 <span class="titulo">Dirección: </span><span>' + this.direccion + '</span></br> \
                 <span class="titulo">Delegación: </span><span>' + this.delegacion + '</span></br>';
-    if (this.telefono) {
-        html += '<span class="titulo">Teléfono: </span>' + this.telefono + '</span></br> \
+        if (this.telefono) {
+            html += '<span class="titulo">Teléfono: </span>' + this.telefono + '</span></br> \
                  <p><a href="tel:' + this.telefono + '"><button data-role="button" data-icon="phone">Llamar</button></a></p>';
-    }
-    html += '<p><a href="#" onclick="window.open(\'' + URL_GMAPS_MAPA + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="location">Ver en mapa</button></a></p> \
+        }
+        html += '<p><a href="#" onclick="window.open(\'' + URL_GMAPS_MAPA + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="location">Ver en mapa</button></a></p> \
             <p><a href="#" onclick="window.open(\'' + URL_GMAPS_DIR + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="navigation">¿Cómo llegar?</button></p> \
             </div></br>';
-    return html;
-};
-function Verificentro() {
+        return html;
+    };
 }
 
-Verificentro.prototype.toHtml = function () {
-    var html = '<div class="cuadro-info"> \
+function Verificentro() {
+    this.toHtml = function () {
+        var html = '<div class="cuadro-info"> \
 		<h4>' + this.razon_social + '</h4> \
 		<span class="titulo">Dirección: </span><span>' + this.direccion + '</span></br> \
 		<span class="titulo">Delegación: </span><span>' + this.delegacion + '</span></br>';
-    if (this.telefono) {
-        html += '<span class="titulo">Teléfono: </span>' + this.telefono + '</span></br> \
+        if (this.telefono) {
+            html += '<span class="titulo">Teléfono: </span>' + this.telefono + '</span></br> \
                 <p><a href="tel:' + this.telefono + '"><button data-role="button" data-icon="phone">Llamar</button></a></p>';
-    }
-    html += '<p><a href="#" onclick="window.open(\'' + URL_GMAPS_MAPA + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="location">Ver en mapa</button></a></p> \
+        }
+        html += '<p><a href="#" onclick="window.open(\'' + URL_GMAPS_MAPA + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="location">Ver en mapa</button></a></p> \
             <p><a href="#" onclick="window.open(\'' + URL_GMAPS_DIR + this.latitud + ',' + this.longitud + '\', \'_system\');"><button data-role="button" data-icon="navigation">¿Cómo llegar?</button></p> \
             </div></br>';
-    return html;
+        return html;
+    };
 }
-;
+
+
+
 function Ubicacion() {
+    this.obtener = function () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(exitoCoordenadas, coordenadasNativo, {enableHighAccuracy: false, timeout: 60 * 1000, maximumAge: 1000 * 60 * 10});
+        } else {
+            errorGeolocalizacion();
+        }
+
+        this._coordenadasNativo = function () {
+            navigator.geolocation.getCurrentPosition(exitoCoordenadas, errorCoordenadas, {enableHighAccuracy: true, timeout: 60 * 1000 * 2, maximumAge: 1000 * 60 * 10});
+        }
+
+        this._exitoCoordenadas = function (location) {
+            var verificentrosCercanos = verificentrosJson['verificentros'];
+            for (i = 0; i < verificentrosCercanos.length; i++) {
+                var verificentro = verificentrosCercanos[i];
+                verificentro['distancia'] = obtenerDistancia(location.coords.latitude, location.coords.longitude, verificentro['latitud'], verificentro['longitud']);
+            }
+            verificentrosCercanos = verificentrosCercanos.sort(compararDistancia).slice(0, NUM_MAX_RES);
+            if (typeof (Storage) !== 'undefined') {
+                localStorage.verificentros = JSON.stringify(verificentrosCercanos);
+                $.mobile.changePage('verificentros-listado.html');
+            }
+        }
+
+        this._errorCoordenadas = function () {
+            $.mobile.loading('hide');
+            alert('Err0r! Obtener coordenadas geolocalización');
+            return false;
+        }
+
+        this._errorGeolocalizacion = function () {
+            $.mobile.loading('hide');
+            alert('Err0r! Error geolocalización!');
+            return false;
+        }
+
+        this._obtenerDistancia = function (lat1, lon1, lat2, lon2) {
+            var R = 6371;
+            var dLat = convertirRadianes(lat2 - lat1);
+            var dLon = convertirRadianes(lon2 - lon1);
+            lat1 = convertirRadianes(lat1);
+            lat2 = convertirRadianes(lat2);
+            var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            var d = R * c;
+            return d
+        }
+
+        this._convertirRadianes = function (numero) {
+            return numero * Math.PI / 180;
+        }
+
+        this._compararDistancia = function (a, b) {
+            if (a.distancia < b.distancia)
+                return -1;
+            if (a.distancia > b.distancia)
+                return 1;
+            return 0;
+        }
+
+        this.guardaLocal = function () {
+        }; // asigna ttl y guarda
+
+        this.existeLocal = function () {
+        }; // busca si está guardado
+    };
 }
-
-Ubicacion.obtener = function () {
-};
-Ubicacion.prototype.guardaLocal = function () {
-}; // asigna ttl y guarda
-
-Ubicacion.existeLocal = function () {
-}; // busca si está guardado
 
 function Adeudo() {
-}
-Adeudo.prototype.toHtml = function () {
-    var html = '<div class="cuadro-info"> \
+    this.toHtml = function () {
+        var html = '<div class="cuadro-info"> \
                 <span class="titulo">Año: </span> \
                 <span>' + this.anio + '</span></br> \
                 <span class="titulo">Impuesto: </span> \
@@ -142,17 +203,17 @@ Adeudo.prototype.toHtml = function () {
                 <span class="titulo">Total: </span> \
                 <span class="cacao">' + this.totalTenencia + '</span></br> \
                 </div></br>';
-    return html;
-};
+        return html;
+    };
+}
 function Infraccion() {
     this.pagada = false;
-}
-Infraccion.prototype.toHtml = function () {
-    var textoPagada = 'No';
-    if (this.pagada) {
-        textoPagada = 'Sí';
-    }
-    var html = '<div class="cuadro-info"> \
+    this.toHtml = function () {
+        var textoPagada = 'No';
+        if (this.pagada) {
+            textoPagada = 'Sí';
+        }
+        var html = '<div class="cuadro-info"> \
                 <span class="titulo">Folio: </span><span>' + this.folio + '</span></br> \
                 <span class="titulo">Fecha: </span><span>' + this.fecha + '</span></br> \
                 <span class="titulo">Pagada: </span><span>' + textoPagada + '</span></br> \
@@ -160,5 +221,6 @@ Infraccion.prototype.toHtml = function () {
                 <span class="titulo">Multa en días de salario: </span><span>' + this.sancionDias + '</span></br> \
 		<span class="titulo">Multa estimada: </span><span class="cacao">' + this.sancionMonto + '</span></br> \
                 </div></br>';
-    return html;
-};
+        return html;
+    };
+}

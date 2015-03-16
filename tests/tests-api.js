6e9fc3df391c2f4daba847782e862a5caedff760
ivansabik <ivan.rodriguez.o@gmail.com>
2015-03-16 14:26:31 -0600

    travis, nodedunit

---
+++ tests/tests-api.js
@@ -0,0 +1,22 @@
+module.exports = {
+    setUp: function testsUnitariosApiSetup(cb) {
+    },
+    tearDown: function testsUnitariosApiTearDown(cb) {
+    },
+    consultaVehiculoInfoGeneral: function(test) {
+    },
+    consultaVehiculoInfracciones: function(test) {
+    },
+    consultaVehiculoAdeudosTenencia: function(test) {
+    },
+    consultaVehiculoPlacasInvalidas: function(test) {
+    },
+    consultaVehiculoHoyNoCircula: function(test) {
+    },
+    consultaVehiculoNoExisteEnPadron: function(test) {
+    },
+    consultaVerificentros: function(test) {
+    },
+    consultaCorralones: function(test) {
+    }
+};

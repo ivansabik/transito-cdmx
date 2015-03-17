c68c9be6dbb601fd581c6f4194df495ebf35e568
ivansabik <ivan.rodriguez.o@gmail.com>
2015-03-17 11:37:25 -0600

    nodeunit algunos tests test.done()

--- tests/tests-api.js
+++ tests/tests-api.js
@@ -7,58 +7,73 @@ module.exports = {
     },
     obtenerPlacas: function(test) {
         assert.equal('183YTP', this.respuesta['placas']);
+        test.done();
     },
     obtenerModelo: function(test) {
         assert.equal(2012, this.respuesta['modelo']);
+        test.done();
     },
     obtenerNumeroCilindros: function(test) {
-        assert.equal(4, this.respuesta['numCilindros']);
+        assert.equal(4, this.respuesta['num_cilindros']);
+        test.done();
     },
     obtenerProcedenciaNacional: function(test) {
-        assert.equal(true, this.respuesta['esNacional']);
+        assert.equal(true, this.respuesta['procedencia_nacional']);
+        test.done();
     },
     obtenerValorFactura: function(test) {
-        assert.equal(615288, this.respuesta['valorFactura']);
+        assert.equal(615288, this.respuesta['valor_factura']);
+        test.done();
     },
     obtenerClaveVehicular: function(test) {
-        assert.equal('0044801', this.respuesta['claveVehicular']);
+        assert.equal('0044801', this.respuesta['clave_vehicular']);
+        test.done();
     },
-    obtenerMontoAdeudosInfracciones: function(test) {
-        assert.equal('', this.respuesta['montoAdeudosInfracciones']);
+    obtenerMontoAdeudos: function(test) {
+        assert.equal('', this.respuesta['monto_adeudos']);
     },
-    obtenerMontoTotalAdeudos: function(test) {
-        assert.equal('', this.respuesta['montoTotalAdeudos']);
-    },
-    obtenerMontoTotalInfracciones: function(test) {
-        assert.equal('', this.respuesta['montoTotalInfracciones']);
+    obtenerMontoInfracciones: function(test) {
+        assert.equal('', this.respuesta['monto_infracciones']);
     },
     obtenerFechaFactura: function(test) {
-        assert.equal('2012-11-30', this.respuesta['fechaFactura']);
+        assert.equal('2012-11-30', this.respuesta['fecha_factura']);
+        test.done();
     },
     obtenerRfc: function(test) {
         assert.equal('Persona Moral', this.respuesta['rfc']);
+        test.done();
     },
     obtenerDepreciacion: function(test) {
         assert.equal(369172.8, this.respuesta['depreciacion']);
-    },
-    obtenerInfracciones: function(test) {
-        assert.equal('', this.respuesta['infracciones']);
-    },
-    obtenerAdeudosTenencia: function(test) {
-        assert.equal('', this.respuesta['adeudosTenencia']);
-    },
-    obtenerMontoTotalAdeudosTenencia: function(test) {
-        assert.equal('', this.respuesta.montoAdeudosTenencias);
+        test.done();
     },
     validarPlacas: function(test) {
         test.throws(function () { this.tdf.consultaPlacas('183-YTP'); });
+        test.done();
     },
     calcularHoyNoCircula: function(test) {
         var assertCalendario = {};
         assert.equal(assertCalendario, this.respuesta['calendarioNoCircula']);
+        test.done();
     },
     buscarVehiculoNoExisteEnPadron: function(test) {
-        test.throws(function () { this.tdf.consultaPlacas(''); });
+        test.throws(function () { this.tdf.consultaPlacas('154DBH'); });
+        test.done();
+    },
+    obtenerMontoAdeudosInfracciones: function(test) {
+        assert.equal('', this.respuesta['monto_adeudos_infracciones']);
+    },
+    obtenerInfracciones: function(test) {
+        assert.equal('', this.respuesta['infracciones']);
+        test.done();
+    },
+    obtenerAdeudosTenencia: function(test) {
+        assert.equal('', this.respuesta['adeudos_tenencia']);
+        test.done();
+    },
+    obtenerMontoAdeudosTenencia: function(test) {
+        assert.equal('', this.respuesta['monto_adeudos_tenencias']);
+        test.done();
     },
     obtenerListaVerificentros: function(test) {
         var assertVerificentros = {};
